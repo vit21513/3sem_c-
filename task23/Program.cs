eb45a4c38cfd105d536ba248a2b4d19d060ed0af
«vitaly» <vit21513@yandex.ru>
2022-07-16 09:18:31 +0300

    add task23

---
+++ task23/Program.cs
@@ -0,0 +1,23 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+
+
+Console.Write("Введите число:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+SquareTable(n);
+
+
+void SquareTable(int n)
+{
+    int i = 1;
+
+    while (i <= n)
+    {
+        Console.WriteLine(" --------");
+        Console.WriteLine($"| {i} | {i * i * i,2} |");
+        i++;
+    }
+}
